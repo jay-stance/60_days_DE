ea20779b86242918f7202985fab9531cbcd1f021
Jau Stance <jaystance25@gmail.com>
2026-02-12 16:37:19 +0100

    first commit

---
+++ day_1_thur/iterators_generators.py
@@ -0,0 +1,3 @@
+# TODO: write a function that takes in a sentence, then makes it an iterable of words, while also adding the __rep__, and __len__ methods
+
+# TODO: creat an iterator from a string and using a while loop, print the values until it raises stop iterator error 
