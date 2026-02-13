0ec770015e6569ad0ec85f76464dc8c497a2eac2
Jau Stance <jaystance25@gmail.com>
2026-02-13 20:56:51 +0100

    pydantic basics

--- day_2_fri/pydantic_validation.py
+++ day_2_fri/pydantic_validation.py
@@ -21,3 +21,55 @@ print(m.dimensions)
 #> (10, 20)
 
 Delivery(timestamp='2020-01-02T03:04:05Z', dimensions=['10'])
+
+
+# =====================================
+
+#     ACTUAL LEARNING STARTS HERE
+
+# =====================================
+
+from pydantic import BaseModel
+
+class User(BaseModel):
+  id: int
+  username: str 
+  is_active: bool 
+  
+messy_obj = {
+  "id": "123",
+  "username": "jay stance",
+  "is_active": "yes"
+}
+
+bad_messy_data = {
+  "id": "lsknosi",
+  "username": "jay stance",
+  "is_active": "yes"
+} 
+
+try:
+  user_data = User(**bad_messy_data)
+  print(user_data)
+except ValidationError as e:
+  print(e)
+  
+
+user_data = User(**messy_obj)
+print(user_data)
+
+
+from pydantic import BaseModel 
+
+class Employee(BaseModel):
+  id: int 
+  name: str
+  department: str = "Software Engineering"
+  manager_name: str | None = None
+  
+new_employee = Employee(id=123, name='jay stance')
+print(new_employee)
+
+print(new_employee.model_dump())
+
+print(new_employee.model_dump_json())
--- day_2_fri/tempCodeRunnerFile.py
+++ day_2_fri/tempCodeRunnerFile.py
@@ -1,1 +1,14 @@
-logfire
+from pydantic import BaseModel 
+
+class Employee(BaseModel):
+  id: int 
+  name: str
+  department: str = "Software Engineering"
+  manager_name: str | None = None
+  
+new_employee = Employee(id=123, name='jay stance')
+print(new_employee)
+
+print(new_employee.model_dump())
+
+print(new_employee.model_dump_json())
