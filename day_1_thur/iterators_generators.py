036f75edba2fe91f0996b76fe157a96bb740558f
Jau Stance <jaystance25@gmail.com>
2026-02-13 14:20:02 +0100

    the infinite generators

--- day_1_thur/iterators_generators.py
+++ day_1_thur/iterators_generators.py
@@ -162,3 +162,25 @@ print(list(filterRes))
 print(next(filterRes))
 print(next(filterRes))
 print(next(filterRes))
+
+
+# ===============================
+
+#     MERGING GENERATORS
+
+# ===============================
+
+import itertools
+
+merged_chain = itertools.chain('ABC', range(2))
+
+print(merged_chain)
+print(list(merged_chain))
+
+# merge_zip = zip(range(4), "abcdef")
+merge_zip = itertools.zip_longest(range(4), "abcdef", fillvalue="Nothing here")
+print(merge_zip)
+print(list(merge_zip))
+
+merge_product = itertools.product('abc', repeat=2)
+print(list(merge_product))
