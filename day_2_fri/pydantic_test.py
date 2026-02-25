b6deee5b544d9d134816f7d04665e03fbdebdfca
Jau Stance <jaystance25@gmail.com>
2026-02-25 21:00:28 +0100

    started day 8 aws security and iam

--- day_2_fri/pydantic_test.py
+++ day_2_fri/pydantic_test.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field, ConfigDict, field_validator, model_validator
 from typing import Annotated
+from datetime import datetime
 
 # test 1
 class Customer(BaseModel):
@@ -136,3 +137,145 @@ class DataBaseConfig(BaseSettings):
 
 config = DataBaseConfig()
 password = config.db_password.get_secret_value()  # how to get the actual password
+
+
+# test 11
+class LogEvent(BaseModel):
+  
+  message: str
+  created_at: datetime = Field(default_factory=datetime.now)
+  
+
+# test 12
+
+from pydantic import computed_field
+
+class Employee(BaseModel):
+  first_name: str
+  last_name: str
+  
+  @computed_field
+  @property
+  def full_name(self): 
+    return f"${self.first_name} ${self.last_name}"
+  
+emp = Employee(first_name="Jane", last_name="Doe")
+print(emp.full_name)  # Jane Doe
+
+
+# test 13
+
+class RawDataRow(BaseModel):
+    category: str
+
+    @model_validator(mode="after")
+    def trim_category(self):
+        # Strip leading/trailing whitespace
+        self.category = self.category.strip()
+        return self
+      
+
+# test 14
+
+class AuditLog(BaseModel):
+  model_config = {"frozen": True}
+  
+  event: str
+  created_at: datetime
+  
+  
+# test 15
+
+from pydantic import BaseModel, field_validator
+from uuid import UUID, uuid4
+
+class Session(BaseModel):
+    session_id: UUID
+
+    @field_validator("session_id")
+    @classmethod
+    def ensure_uuid4(cls, value: UUID):
+        if value.version != 4:
+            raise ValueError("session_id must be a UUID4")
+        return value
+      
+      
+# test 16
+
+class PatientRecord(BaseModel):
+  patient_id: str
+  name: str
+  social_security: str 
+  
+p1_record = PatientRecord("idsois", "Edu Brazil", "hh667")
+p1_record.model_dump(exclude={"social_security"})
+
+# test 17
+
+# Custom HexColor type: string starting with '#' and exactly 7 chars
+HexColor = Annotated[str, Field(regex=r"^#[0-9A-Fa-f]{6}$")]
+
+class Theme(BaseModel):
+    color_code: HexColor
+    
+    
+# task 18
+
+class ServerNode(BaseModel):
+    node_id: str
+    status: str = "offline"
+    
+node1 = ServerNode(node_id="node-123")
+print(node1)
+
+node2 = node1.model_copy(update={"status": "online"}) 
+
+# whenever you pass something inside all these model functions, the arguments usually uses diction/set symbols
+
+
+# test 19
+
+from typing import Union, Annotated, Literal
+
+class PageView(BaseModel):
+    event_type: Literal["view"]
+    url: str
+
+class Click(BaseModel):
+    event_type: Literal["click"]
+    button_id: str
+    
+StreamEvent = Annotated[
+    Union[PageView, Click],
+    Field(discriminator="event_type")
+]
+  
+  
+# test 20
+
+from pydantic import BaseModel, Field, field_validator
+from typing import Annotated, List
+
+
+class Metadata(BaseModel):
+    tags: List[str]
+    source: str = "unknown"
+
+    @field_validator("tags", mode="before")
+    @classmethod
+    def lowercase_tags(cls, value):
+        return [tag.lower() for tag in value]
+
+
+class ValidateRequest(BaseModel):
+    request_id: Annotated[str, Field(alias="req-id")]
+    metadata: Metadata
+    scores: List[float]
+
+    @field_validator("scores")
+    @classmethod
+    def validate_scores(cls, value):
+        for score in value:
+            if score > 100:
+                raise ValueError("Scores cannot exceed 100")
+        return value
