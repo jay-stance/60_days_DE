655dfb190aff2809dacab261520b999f61d9e1e5
Jau Stance <jaystance25@gmail.com>
2026-03-06 16:41:32 +0100

    added labels, output

--- day_10_terraform_i_as_code/main.tf
+++ day_10_terraform_i_as_code/main.tf
@@ -2,14 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "enviroment" {
+variable "environment" {
   description = "enviroment (dev, staging, prod etc)"
   type = string
   default = "dev"
 }
 
+locals {
+  team_name   = "data-engineering"
+  project     = "tachpae-core"
+  # You can combine strings dynamically here
+  bucket_name = "${local.project}-${var.environment}-logs"
+}
+
 resource "aws_s3_bucket" "tachpe_logs_bucket" {
-  bucket = "tachpae-${var.enviroment}-logs-bucket"
+  # bucket = "tachpae-${var.environment}-logs-bucket"
+
+  bucket = local.bucket_name
+  
+  tags = {
+    Team        = local.team_name
+    Environment = var.environment
+  }
 }
 
 resource "aws_iam_policy" "iam_policy" {
@@ -17,15 +31,20 @@ resource "aws_iam_policy" "iam_policy" {
   description = "this allows de to read logs bucket"
 
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [{
-      "Sid": "statemtn1",
-      "Effect": "Allow",
-      "Action": ["s3:ListBucket", "s3:GetObject"],
-      "Resource": [
+    Version: "2012-10-17",
+    Statement: [{
+      Sid: "statemtn1",
+      Effect: "Allow",
+      Action: ["s3:ListBucket", "s3:GetObject"],
+      Resource: [
         aws_s3_bucket.tachpe_logs_bucket.arn,
         "${aws_s3_bucket.tachpe_logs_bucket.arn}/*"
       ]
     }]
   })
+}
+
+output "s3_bucket_arn" {
+  description = "The ARN of the newly created logs bucket"
+  value = aws_s3_bucket.tachpe_logs_bucket.arn
 }
