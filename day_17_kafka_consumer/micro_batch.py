08724d6fa8c1e698410a6b837ee1bdbfce717e8a
Jau Stance <jaystance25@gmail.com>
2026-03-20 14:22:32 +0100

    added on revoke for when kafka wants to trigger rebalance for partitions

--- day_17_kafka_consumer/micro_batch.py
+++ day_17_kafka_consumer/micro_batch.py
@@ -1,3 +1,39 @@
+from confluent_kafka import Consumer
+import json 
+import psycopg2
+
+# 1. Connect to the Database (External Sink)
+db_conn = psycopg2.connect("dbname=iot_db user=admin password=secret")
+cursor = db_conn.cursor()
+
+
+def on_revoke(consumer, partitions):
+    print("Kafka is taking my partition! Triggering emergency flush...")
+    
+    if len(valid_records) > 0:
+        # 1. Save the data to the database
+        cursor.executemany(upsert_query, valid_records)
+        db_conn.commit()
+        
+        # 2. Tell Kafka we successfully processed this final batch
+        # We find the very last message in our batch and commit its high-water mark
+        last_msg = valid_records_raw_messages[-1] 
+        consumer.commit(message=last_msg)
+        
+        # 3. Clear our memory
+        valid_records.clear()
+        print("Data saved and offset committed. Partition handed back safely.")
+
+config = {
+    'bootstrap.servers': 'pkc-YOUR-ENDPOINT.confluent.cloud:9092',
+    'group.id': 'thermostat_scaling_group', # <-- THIS IS THE MAGIC KEY
+    'auto.offset.reset': 'earliest'
+}
+
+consumer = Consumer(config)
+consumer.subscribe(['thermostat_readings'], on_revoke=on_revoke)
+
+
 # The Micro-Batching Consumer Pattern
 consumer.subscribe(['thermostat_readings'])
 
